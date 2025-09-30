d6cd34fa25c3a939a82787afd28a4ff4c52e0101
Florent Mineiro <florent.mineiro.93@gmail.com>
2025-09-30 09:55:45 +0200

    Création Class Film

---
+++ Film.php
@@ -0,0 +1,32 @@
+<?php
+
+class Film 
+{
+   private int $numeroFilm ;
+   private string  $titreFilm ;
+   private int $nbEntreeFilm ;
+
+   public function __construct(int $numeroFilm ,string $titreFilm, int $nbEntreeFilm)
+   {
+    $this-> numeroFilm = $numeroFilm ;
+    $this-> titreFilm = $titreFilm ;
+    $this-> nbEntreeFilm = $nbEntreeFilm ;
+   }
+
+   public function getNumeroFilm():int
+   {
+      return this -> $numeroFilm;
+   }
+    public function getTitreFilm(): string
+   {
+      return this -> $titreFilm;
+   }
+    public function getNbEntreeFilm(): int
+   {
+      return this -> $nbEntreeFilm;
+   }
+   public function setNbEntreeFilm(int $nbEntreeFilm) : void
+   {
+      $this-> nbEntreFilm = $nbEntreeFilm;
+   }
+}
