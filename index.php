c45c85dcd1825a7918d96dd02b60253400d650f0
Florent Mineiro <florent.mineiro.93@gmail.com>
2025-10-07 22:41:52 +0200

    fini

--- index.php
+++ index.php
@@ -10,20 +10,25 @@ $premierRealisateur = new Realisateur("Jimenez","Cedric");
 $deuxiemeRealisateur = new Realisateur("Gozian","Yann");
 $troisiemeRealisateur = new Realisateur("Reeves","Matt");
 
+
 $premierFilm = new Film(1,"Bac Nord",563921,$premierRealisateur);
 $deuxiemeFilm = new Film(2,"Boite Noire",432789,$deuxiemeRealisateur);
 $troisiemeFilm = new Film(3,"The Batman",879214,$troisiemeRealisateur);
 $premierFilm->setNbEntreeFilm(789345);
+
 
 $premierActeur = new Acteur("Lellouche","Gilles",false);
 $deuxiemeActeur = new Acteur("Civil","François",false);
 $troisiemeActeur = new Acteur("Leklou","Karim",true);
 $quatriemeActeur = new Acteur("Niney","Pierre",true);
 $cinquiemeActeur = new Acteur("De Laâge","Lou",false);
-$sixiemeActeur = new Acteur("Dussolier","André",true);
-$septiemeActeur = new Acteur("Pattinson","Robert",true);
+$sixiemeActeur = new Acteur("Dussolier","André",false);
+$septiemeActeur = new Acteur("Pattinson","Robert",false);
 $huitiemeActeur = new Acteur("Kravitz","Zöe",false);
 $neuviemeActeur = new Acteur("Dano","Paul",false);
+
+
+
 
 $premierFilm->ajouterActeur($premierActeur);
 $premierFilm->ajouterActeur($deuxiemeActeur);
@@ -45,21 +50,25 @@ echo "Liste des Acteurs ==> \n";
 
 foreach($premierFilm-> getLesActeurs() as $acteurs)
 {
-        echo $acteurs->getNomActeur()." - ".$acteurs->getPrenomActeur()." - ".$acteurs->getEstCesarise()."\n";
+        echo $acteurs->getNomActeur()." - ".$acteurs->getPrenomActeur()." - ";
+        echo ($acteurs->getEstCesarise() ? 'true':'false')."\n";
 }
 
-echo "Info sur le film ==> \n".$deuxiemeFilm->getNumeroFilm()." - ".$deuxiemeFilm ->getTitreFilm()." - ".$deuxiemeFilm->getNbEntreeFilm().$deuxiemeRealisateur->getNomRealisateur()." - ".$deuxiemeRealisateur->getPrenomRealisateur()."\n";
+echo "Info sur le film ==> \n".$deuxiemeFilm->getNumeroFilm()." - ".$deuxiemeFilm ->getTitreFilm()." - ".$deuxiemeFilm->getNbEntreeFilm()." - ".$deuxiemeRealisateur->getNomRealisateur()." - ".$deuxiemeRealisateur->getPrenomRealisateur()."\n";
 echo "Liste des Acteurs ==> \n";
+
 foreach($deuxiemeFilm-> getLesActeurs() as $acteursDeuxiemeFilm)
 {
-        echo $acteursDeuxiemeFilm->getNomActeur()." - ".$acteursDeuxiemeFilm->getPrenomActeur()." - ".$acteursDeuxiemeFilm->getEstCesarise()."\n";
-}
+        echo $acteursDeuxiemeFilm->getNomActeur()." - ".$acteursDeuxiemeFilm->getPrenomActeur()." - ";
+        echo ($acteursDeuxiemeFilm->getEstCesarise() ? 'true ':'false')."\n";
+}       
 
 
-echo "Info sur le film ==> \n".$troisiemeFilm->getNumeroFilm()." - ".$troisiemeFilm->getTitreFilm()." - ".$troisiemeFilm->getNbEntreeFilm().$troisiemeRealisateur->getNomRealisateur()." - ".$troisiemeRealisateur->getPrenomRealisateur();
+echo "Info sur le film ==> \n".$troisiemeFilm->getNumeroFilm()." - ".$troisiemeFilm->getTitreFilm()." - ".$troisiemeFilm->getNbEntreeFilm().$troisiemeRealisateur->getNomRealisateur()." - ".$troisiemeRealisateur->getPrenomRealisateur()."\n";
 echo "Liste des Acteurs ==> \n";
 
 foreach($troisiemeFilm-> getLesActeurs() as $acteursTroisiemeFilm)
 {
-        echo $acteursTroisiemeFilm->getNomActeur()." - ".$acteursTroisiemeFilm->getPrenomActeur()." - ".$acteursTroisiemeFilm->getEstCesarise()."\n";
+        echo $acteursTroisiemeFilm->getNomActeur()." - ".$acteursTroisiemeFilm->getPrenomActeur()." - ";
+        echo $acteursTroisiemeFilm->getEstCesarise() ? 'true':'false'."\n";
 }
